9acf066ab418d47a304a917922a6485da914d938
riemann <aleqvids@gmail.com>
2023-07-04 05:47:52 +0300

    fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,33 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "@/lib/prisma";
-import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
+
+
+
+
+import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID as string,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      }),
+    // ...add more providers here
   ],
+
+  // A database is optional, but required to persist accounts in a database
+  database: process.env.DATABASE_URL,
 };
 
 const handler = NextAuth(authOptions);
 
+
+
+
+
 export { handler as GET, handler as POST };
+
+
