cd0b19c8eaab9b9e01793cc0605a1e321afba76f
riemann <aleqvids@gmail.com>
2023-08-20 09:53:19 +0300

    update link

--- app/page.tsx
+++ app/page.tsx
@@ -54,7 +54,7 @@ export default async function Home() {
 
           <a
             className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800"
-            href="https://github.com/partylikeits1983"
+            href="https://github.com/Chess-Fish"
             target="_blank"
             rel="noopener noreferrer"
           >
